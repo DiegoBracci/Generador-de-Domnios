b0b77da8e99593719ddb0906f01ac42e60aaae3e
DiegoBracci <dbraccipro@gmail.com>
2020-12-09 19:53:26 +0000

    Final-Proyect

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-var pronoun = ['the','our'];
+  var pronoun = ['the','our'];
   var adj = ['great', 'big' ];
   var noun = ['jogger','racoon'];
   var dom = [ ".com" , ".es"]
 
-  for ( let k in pronoun){
-      for(let Y in adj){
-          for( let x in noun){
-              for(let z in dom){
-                  console.log(pronoun[k] + adj[Y] + noun[x] + dom[z])
-              }
-          }
-      }
-  }
+            for ( let k in pronoun)
+            {
+                for(let Y in adj)
+                {
+                    for( let x in noun)
+                    {
+                        for(let z in dom)
+                        {
+                            console.log(pronoun[k] + adj[Y] + noun[x] + dom[z])
+                        }
+                    }
+                }
+            }
